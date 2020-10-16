b78e27217d51eb25de688f56fbec9532ef986296
Dihak <dihak@digitalkode.com>
2020-10-16 07:33:13 +0700

    Add modal habit

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:istiqomah/constants/font_family.dart';
 
 final ThemeData themeData = new ThemeData(
-    fontFamily: FontFamily.productSans,
-    brightness: Brightness.light,
-    primaryColor: Colors.blueAccent[700],
-    primaryColorBrightness: Brightness.light,
-    accentColor: Colors.blueAccent,
-    accentColorBrightness: Brightness.light,
-    backgroundColor: Colors.blueAccent[700]);
+  fontFamily: FontFamily.productSans,
+  brightness: Brightness.light,
+  primaryColor: Color(0xff2F80ED),
+  primaryColorBrightness: Brightness.light,
+  accentColor: Colors.blueAccent,
+  accentColorBrightness: Brightness.light,
+  backgroundColor: Colors.blueAccent[700],
+  canvasColor: Colors.transparent,
+);
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,6 +4,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Theme.of(context).primaryColor,
       body: _buildBody(context),
     );
@@ -22,7 +23,7 @@ class Home extends StatelessWidget {
     );
   }
 
-  Widget _header(context) {
+  Widget _header(BuildContext context) {
     return Container(
       width: double.infinity,
       child: Row(children: [
@@ -40,7 +41,9 @@ class Home extends StatelessWidget {
           fillColor: Colors.white,
           shape: new CircleBorder(),
           constraints: BoxConstraints.expand(width: 50, height: 50),
-          onPressed: () {},
+          onPressed: () {
+            _addHabit(context);
+          },
           child: Icon(
             Icons.add,
             color: Theme.of(context).primaryColor,
@@ -57,4 +60,100 @@ class Home extends StatelessWidget {
       ),
     );
   }
+}
+
+void _addHabit(context) {
+  showModalBottomSheet(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(40.0),
+      ),
+    ),
+    backgroundColor: Theme.of(context).primaryColor,
+    context: context,
+    isScrollControlled: true,
+    builder: (context) => Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Text(
+              'Tambah kebiasaan',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5),
+            child: Text(
+              'Nama',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0x33FFFFFF),
+                border: OutlineInputBorder(borderSide: (BorderSide.none)),
+              ),
+              style: TextStyle(color: Color(0xFFFFFFFF)),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Center(
+              child: FlatButton(
+                color: Colors.white,
+                textColor: Theme.of(context).primaryColor,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                splashColor: Colors.blue[50],
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                child: Text(
+                  "OK",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Batal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
