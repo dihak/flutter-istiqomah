8971d2883ecc2c81ff4e5cccb13b649d3d936845
Dihak <dihak@digitalkode.com>
2020-10-30 17:30:03 +0700

    Add monthShortName and dayShortName in constant

---
+++ lib/constants/app.dart
@@ -0,0 +1,24 @@
+final List<String> monthShortName = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+final List<String> dayShortName = [
+  "Sen",
+  "Sel",
+  "Rab",
+  "Kam",
+  "Jum",
+  "Sab",
+  "Min"
+];
