c3f4731848d91d29e6c2457302ee6a7e7d9410a9
Dihak <dihak@digitalkode.com>
2020-11-19 10:24:11 +0700

    Add notification model

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:istiqomah/constants/app_theme.dart';
 import 'package:istiqomah/models/habit.dart';
+import 'package:istiqomah/models/notification.dart';
 import 'package:istiqomah/routes.dart';
 import 'package:istiqomah/pages/get_started/index.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => HabitModel(),
-        child: IstiqomahApp(),
-      ),
-    );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize flutter notification
+  initializeNotification();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => HabitModel(),
+      child: IstiqomahApp(),
+    ),
+  );
+}
 
 class IstiqomahApp extends StatelessWidget {
   @override
---
+++ lib/models/notification.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class ReceivedNotification {
+  ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+}
+
+const MethodChannel platform =
+    MethodChannel('dexterx.dev/flutter_local_notifications_example');
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future initializeNotification() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('ic_launcher_foreground');
+
+  /// Note: permissions aren't requested here just to demonstrate that can be
+  /// done later
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+  });
+
+  tz.initializeTimeZones();
+}
+
+class NotificationModel {
+  static final FlutterLocalNotificationsPlugin notif =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> showNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+            'habit-notif', 'Pengingat', 'Pengingat aktivitas harian',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await notif.show(0, title, body, platformChannelSpecifics,
+        payload: 'item x');
+  }
+
+  static scheduleNotification() async {
+    print(tz.local);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+        const NotificationDetails(
+            android: AndroidNotificationDetails('your channel id',
+                'your channel name', 'your channel description')),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime);
+  }
+}
