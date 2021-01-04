452f664b9584c2a409e4d6188a5276a6459be8a8
Mohit <mohit2002ss@gmail.com>
2021-01-04 23:45:26 +0530

    Brought back blue theme

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _IstiqomahAppState extends State<IstiqomahApp> {
     return MaterialApp(
       title: 'ISTIQOMAH',
       routes: Routes.routes,
-      theme: darkTheme,
+      theme: blueTheme,
       home: Scaffold(
         body: _body,
       ),
