da631b49e93ac4a4d3e571a0e5da6da6ceb8c006
Mohit <mohit2002ss@gmail.com>
2021-01-09 18:56:56 +0530

    added languages

---
+++ lib/pages/settings/languages/language_en.dart
@@ -0,0 +1,53 @@
+import 'language.dart';
+
+class LanguageEN extends Languages {
+  @override
+  String get remainder => "Reminder";
+  @override
+  String get delete => "Delete";
+  @override
+  String get everyday => "Everyday";
+  @override
+  String get day1 => "Mon";
+  @override
+  String get day2 => "Tue";
+  @override
+  String get day3 => "Wed";
+  @override
+  String get day4 => "Thur";
+  @override
+  String get day5 => "Fri";
+  @override
+  String get day6 => "Sat";
+  @override
+  String get day7 => "Sun";
+  @override
+  String get stats => "Statistics";
+  @override
+  String get queDelete => "Are you sure you want to Delete ";
+  @override
+  String get cancel => "Cancel";
+  @override
+  String get cngHabit => "Change Habit";
+  @override
+  String get habitName => "Name";
+  @override
+  String get ok => "OK";
+  @override
+  String get intro1 => "Helping you achieve life resolution";
+  @override
+  String get intro2 => "Set the type of activity, with the reminder feature";
+  @override
+  String get intro3 =>
+      "Get a summary of the data during your Istiqomah journey";
+  @override
+  String get start => "Start";
+  @override
+  String get noHabit => "You have no Habit, build your Habit Now!";
+  @override
+  String get getHabit => "Get into a Habit";
+  @override
+  String get settingTitle => "Settings";
+  @override
+  String get themeChange => "Theme";
+}
