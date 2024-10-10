fe3b38e9dd27618d7b195e8fc2fb74ca789e77eb
sdheer <shreekanthd@gmail.com>
2024-10-10 14:21:00 +0800

    Remove button added to detailed view top right corner

--- lib/pages/detail/_header.dart
+++ lib/pages/detail/_header.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:istiqomah/models/habit.dart';
+import 'package:istiqomah/pages/detail/modal/delete.dart';
 import 'package:istiqomah/pages/detail/modal/edit.dart';
+import 'package:provider/provider.dart';
 
 class Header extends StatelessWidget {
-  Header(this.habit, {this.onChange});
+  const Header(this.habit, {this.onChange,this.onRemove});
 
   final Habit habit;
   final Function(Habit)? onChange;
+  final Function(Habit)? onRemove;
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +47,21 @@ class Header extends StatelessWidget {
                 }
               },
             );
+          },
+        ),
+        IconButton(
+          padding: EdgeInsets.all(0),
+          constraints: BoxConstraints(),
+          color: Colors.white,
+          icon: const Icon(Icons.delete),
+          onPressed: () {
+            modalDeleteHabit(context, habit.name).then((value) {
+          if (value!) {
+            Navigator.pop(context);
+
+            onRemove!(habit);
+          }
+        });
           },
         ),
       ],
