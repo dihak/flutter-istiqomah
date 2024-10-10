b704b8366c7d7a2db89a4f6b3c457ed1a972e94f
sdheer <shreekanthd@gmail.com>
2024-10-10 14:29:03 +0800

    removed unwanted references

--- lib/pages/detail/_header.dart
+++ lib/pages/detail/_header.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:istiqomah/models/habit.dart';
-import 'package:istiqomah/pages/detail/modal/delete.dart';
 import 'package:istiqomah/pages/detail/modal/edit.dart';
-import 'package:provider/provider.dart';
 
 class Header extends StatelessWidget {
   const Header(this.habit, {this.onChange,this.onRemove});
